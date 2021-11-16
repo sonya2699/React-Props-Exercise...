269fa28bbea719f18a835de70109bc77a18e6df7
Student <student@Cumberlandss-MacBook-Pro.local>
2021-11-16 01:34:10 -0600

    corrected some things

--- src/exercise/Exercise.js
+++ src/exercise/Exercise.js
@@ -4,8 +4,8 @@ import Bonus from "../bonus/Bonus";
 function Exercise ({ good="Great", bad="Not Great"}){
 return(
 <div>
-  <h1>good</h1>
-  <h1>bad</h1>
+  <h1>{good}</h1>
+  <h1>{bad}</h1>
   <Bonus/>
 </div>
 );
